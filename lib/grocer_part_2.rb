422817090c3be9a87218aa0e3b127420fdd5e4e9
Blake Runyon <blakerunyon09@gmail.com>
2021-01-07 22:51:50 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -23,6 +23,7 @@ def apply_coupons(cart, coupons)
     item = find_item_by_name_in_collection(coupon[:item], cart)
       if item[:count] > coupon[:num]
         item[:count] -= coupon[:num]
+        coupon[:item] = #{coupon[:item] W/COUPON}
         new_cart.push(coupon)
       elsif item [:count] === coupon[:num]
         p "Does Not Apply"
