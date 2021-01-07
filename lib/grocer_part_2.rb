9289c1b8764409d47e882968d9ccbe0071533dfd
Blake Runyon <blakerunyon09@gmail.com>
2021-01-07 23:28:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,8 +13,8 @@ def apply_coupons(cart, coupons)
   # cart = [{:item=>"AVOCADO", :price=>3.0, :clearance=>true, :count=>3},{:item=>"KA...}]
   coupons.map { |coupon|
     if find_item_by_name_in_collection(coupon[:item], cart)
-      p "Got it"
     item = find_item_by_name_in_collection(coupon[:item], cart)
+    p item
       if item[:count] >= coupon[:num]
         item[:count] -= coupon[:num]
         coupon[:item] = "#{coupon[:item]} W/COUPON"
