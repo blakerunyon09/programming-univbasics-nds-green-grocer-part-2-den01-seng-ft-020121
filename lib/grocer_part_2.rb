1a4747c3e21324c912f8c27a66bf5c96ab929fa6
Blake Runyon <blakerunyon09@gmail.com>
2021-01-07 23:38:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -30,9 +30,8 @@ def apply_coupons(cart, coupons)
         coupon.delete(:cost)
         cart.push(coupon)
       end
-        if item[:count] === 0
-          cart.delete(item)
-        end
+      if item[:count] === 0
+        cart.delete(item)
       end
     end
   }
