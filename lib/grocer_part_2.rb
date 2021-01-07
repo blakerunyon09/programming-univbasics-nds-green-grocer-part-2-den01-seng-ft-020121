a98cda4c9a97b423050e764966dc153910049316
Blake Runyon <blakerunyon09@gmail.com>
2021-01-07 23:37:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -11,7 +11,8 @@ cart_test = [
 ]
 
 coupons_test = [
-  {:item => "AVOCADO", :num => 2, :cost => 5.00}
+  {:item => "AVOCADO", :num => 2, :cost => 5.00},
+  {:item => "BEER", :num => 1, :cost => 5.00}
 ]
 
 def apply_coupons(cart, coupons)
