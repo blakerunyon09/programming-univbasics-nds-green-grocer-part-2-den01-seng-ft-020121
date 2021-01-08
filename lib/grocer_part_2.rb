6f351ffcb1050720f6af77a46ae65c26a9ac08df
Blake Runyon <blakerunyon09@gmail.com>
2021-01-08 00:08:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -52,7 +52,7 @@ end
 #apply_clearance(consolidate_cart(cart_test))
 
 def checkout(cart, coupons)
-  consolidate_cart(cart)
+  pp consolidate_cart(cart)
   apply_coupons(cart, coupons)
   apply_clearance(cart)
   pp cart
